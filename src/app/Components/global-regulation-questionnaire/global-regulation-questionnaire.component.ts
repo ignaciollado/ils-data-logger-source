ea60b23c9e19a51938347520d9c99305d4f67e3d
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2023-11-24 09:46:10 +0100

    sigo con los tooltips del cuestionario general

--- src/app/Components/global-regulation-questionnaire/global-regulation-questionnaire.component.ts
+++ src/app/Components/global-regulation-questionnaire/global-regulation-questionnaire.component.ts
@@ -3,17 +3,27 @@ import { EnvironmentalAuditsService } from 'src/app/Services/environmental-audit
 import { QuestionDTO } from 'src/app/Models/question.dto';
 
 import { TooltipPosition } from '@angular/material/tooltip';
-
+import { MtxDialog } from '@ng-matero/extensions/dialog';
+import { DrawerPosition, MtxDrawer, MtxDrawerRef } from '@ng-matero/extensions/drawer';
 
 @Component({
   selector: 'app-global-regulation-questionnaire',
   templateUrl: './global-regulation-questionnaire.component.html',
   styleUrls: ['./global-regulation-questionnaire.component.scss']
 })
+
 export class GlobalRegulationQuestionnaireComponent {
 questionList: QuestionDTO[]
 positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
 position =  this.positionOptions[0];
+mtxDialog: MtxDialog;
+positionDrawer: DrawerPosition = 'right';
+width = '300px';
+height = '300px';
+hasBackdrop = true;
+disableClose = false;
+closeOnNavigation = true;
+  drawer: MtxDrawer;
 
 constructor (
   private enviromentalAuditService: EnvironmentalAuditsService,
@@ -30,4 +40,6 @@ private loadQuestions(): void {
       console.log (this.questionList[3].answers)
     })
 }
+
 }
+

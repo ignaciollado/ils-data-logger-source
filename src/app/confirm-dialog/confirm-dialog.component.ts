289d66bdb8ff8a5a527145489651ee06342dc50c
Ignacio Lladó Vidal <ignacio.llado@idi.es>
2023-12-02 10:13:36 +0100

    he actualizado la ventana de tooltip y los textos de las preguntas añadiendo enlaces y acrónimos

--- src/app/confirm-dialog/confirm-dialog.component.ts
+++ src/app/confirm-dialog/confirm-dialog.component.ts
@@ -7,10 +7,23 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
   styleUrls: ['./confirm-dialog.component.scss']
 })
 export class ConfirmDialogComponent {
+  isPdf1: boolean = false
+  isPdf2: boolean = false
+  
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: {questionText:string, toolTipText: string, doc1: string, doc2: string}
     ) {
-
-      console.log (data.doc1, data.doc2)
+      if (data.doc1) {
+        console.log ( data.doc1.split(".")[1] )
+        if (data.doc1.split(".")[1]==='pdf') {
+          this.isPdf1 = true
+        }
+      }
+      if (data.doc2) {
+        console.log ( data.doc2.split(".")[1] )
+        if (data.doc2.split(".")[1]==='pdf') {
+          this.isPdf2 = true
+        }
+      }      
      }
 }

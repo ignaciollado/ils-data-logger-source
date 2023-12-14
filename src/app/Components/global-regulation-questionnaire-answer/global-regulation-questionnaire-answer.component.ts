34b7c2c50eddcb5184cf3f8d6ba5149f681883f8
Ignacio Lladó Vidal <ignacio.llado@idi.es>
2023-12-14 18:10:25 +0100

    estoy con el cuestionario genérico la parte del cuestionario

--- src/app/Components/global-regulation-questionnaire-answer/global-regulation-questionnaire-answer.component.ts
+++ src/app/Components/global-regulation-questionnaire-answer/global-regulation-questionnaire-answer.component.ts
@@ -33,6 +33,7 @@ export class GlobalRegulationQuestionnaireAnswerComponent {
   }
 
   loadQuestionnaire( questionnaireID: number){
+        console.log (this.regulationsList)
         this.enviromentalAuditService.getQuestionnaireByID( questionnaireID )
           .subscribe( (questionnaires: AnswerDTO[]) => {
             this.userQuestionnaires = questionnaires
@@ -42,64 +43,134 @@ export class GlobalRegulationQuestionnaireAnswerComponent {
                     item.regulation.map( (questions:any) =>{
                       if (questions.q1) {
                         questions.q1.map((q1Reg:any) =>{
-                          console.log("q1",q1Reg)
-                          this.regulationsList.map((regulation:any) => {
-                              if (regulation.reg_ID === q1Reg) {
-                                q1Reg += regulation.reg_ID+"/"+regulation.Ambito+"/"+regulation.Titulo+"/"+regulation.link
+                          this.regulationsList.map((regulation:regulationsDTO) => {
+                            console.log (regulation.reg_ID, q1Reg, ((regulation.reg_ID === q1Reg)))
+                             /*  if (regulation.reg_ID == q1Reg) {
+                                q1Reg = regulation.reg_ID+"/"+regulation.Ambito+"/"+regulation.Titulo+"/"+regulation.link
                                 console.log( regulation.reg_ID, q1Reg)
-                              }
+                              } */
                           }
                           )
                         })
                       }
                       if (questions.q2) {
                         questions.q2.map((q2Reg:any) =>{
-                         //console.log("q2",q2Reg, this.loadRegulationsByID(q2Reg))
+                          this.regulationsList.map((regulation:regulationsDTO) => {
+                            console.log (regulation.reg_ID, q2Reg, ((regulation.reg_ID === q2Reg)))
+                           /*  if (regulation.reg_ID === q2Reg) {
+                              q2Reg = regulation.reg_ID+"/"+regulation.Ambito+"/"+regulation.Titulo+"/"+regulation.link
+                              console.log( regulation.reg_ID, q2Reg)
+                            } */
+                        }
+                        )
                         })
                       }
                       if (questions.q3) {
                         questions.q3.map((q3Reg:any) =>{
-                         //console.log("q3",q3Reg, this.loadRegulationsByID(q3Reg))
+                          this.regulationsList.map((regulation:regulationsDTO) => {
+                            console.log (regulation.reg_ID, q3Reg, ((regulation.reg_ID === q3Reg)))
+                            /* if (regulation.reg_ID === q3Reg) {
+                              q3Reg = regulation.reg_ID+"/"+regulation.Ambito+"/"+regulation.Titulo+"/"+regulation.link
+                              console.log( regulation.reg_ID, q3Reg)
+                            } */
+                        }
+                        )
                         })
                       }
                       if (questions.q4) {
                         questions.q4.map((q4Reg:any) =>{
-                         //console.log("q4",q4Reg, this.loadRegulationsByID(q4Reg))
+                          this.regulationsList.map((regulation:regulationsDTO) => {
+                            console.log (regulation.reg_ID, q4Reg, ((regulation.reg_ID === q4Reg)))
+                           /*  if (regulation.reg_ID === q4Reg) {
+                              q4Reg = regulation.reg_ID+"/"+regulation.Ambito+"/"+regulation.Titulo+"/"+regulation.link
+                              console.log( regulation.reg_ID, q4Reg)
+                            } */
+                        }
+                        )
                         })
                       }
                       if (questions.q5) {
                         questions.q5.map((q5Reg:any) =>{
-                         //console.log("q5",q5Reg, this.loadRegulationsByID(q5Reg))
+                          this.regulationsList.map((regulation:regulationsDTO) => {
+                            console.log (regulation.reg_ID, q5Reg, ((regulation.reg_ID === q5Reg)))
+                          /*   if (regulation.reg_ID === q5Reg) {
+                              q5Reg = regulation.reg_ID+"/"+regulation.Ambito+"/"+regulation.Titulo+"/"+regulation.link
+                              console.log( regulation.reg_ID, q5Reg)
+                            } */
+                        }
+                        )
                         })
                       }
                       if (questions.q6) {
                         questions.q6.map((q6Reg:any) =>{
-                         //console.log("q6",q6Reg, this.loadRegulationsByID(q6Reg))
+                          this.regulationsList.map((regulation:regulationsDTO) => {
+                            console.log (regulation.reg_ID, q6Reg, ((regulation.reg_ID === q6Reg)))
+                           /*  if (regulation.reg_ID === q6Reg) {
+                              q6Reg = regulation.reg_ID+"/"+regulation.Ambito+"/"+regulation.Titulo+"/"+regulation.link
+                              console.log( regulation.reg_ID, q6Reg)
+                            } */
+                        }
+                        )
                         })
                       }
                       if (questions.q7) {
                         questions.q7.map((q7Reg:any) =>{
-                         //console.log("q7",q7Reg, this.loadRegulationsByID(q7Reg))
+                          this.regulationsList.map((regulation:regulationsDTO) => {
+                            console.log (regulation.reg_ID, q7Reg, ((regulation.reg_ID === q7Reg)))
+                          /*   if (regulation.reg_ID === q7Reg) {
+                              q7Reg = regulation.reg_ID+"/"+regulation.Ambito+"/"+regulation.Titulo+"/"+regulation.link
+                              console.log( regulation.reg_ID, q7Reg)
+                            } */
+                        }
+                        )
                         })
                       }
                       if (questions.q8) {
                         questions.q8.map((q8Reg:any) =>{
-                         //console.log("q8",q8Reg, this.loadRegulationsByID(q8Reg))
+                          this.regulationsList.map((regulation:regulationsDTO) => {
+                            console.log (regulation.reg_ID, q8Reg, ((regulation.reg_ID === q8Reg)))
+                           /*  if (regulation.reg_ID === q8Reg) {
+                              q8Reg = regulation.reg_ID+"/"+regulation.Ambito+"/"+regulation.Titulo+"/"+regulation.link
+                              console.log( regulation.reg_ID, q8Reg)
+                            } */
+                        }
+                        )
                         })
                       }
                       if (questions.q9) {  
                         questions.q9.map((q9Reg:any) =>{
-                         //console.log("q9",q9Reg, this.loadRegulationsByID(q9Reg))
+                          this.regulationsList.map((regulation:regulationsDTO) => {
+                            console.log (regulation.reg_ID, q9Reg, ((regulation.reg_ID === q9Reg)))
+                        /*     if (regulation.reg_ID === q9Reg) {
+                              q9Reg = regulation.reg_ID+"/"+regulation.Ambito+"/"+regulation.Titulo+"/"+regulation.link
+                              console.log( regulation.reg_ID, q9Reg)
+                            } */
+                        }
+                        )
                         })
                       }
                       if (questions.q10) {
                         questions.q10.map((q10Reg:any) =>{
-                         //console.log("q10",q10Reg, this.loadRegulationsByID(q10Reg))
+                          this.regulationsList.map((regulation:regulationsDTO) => {
+                            console.log (regulation.reg_ID, q10Reg, ((regulation.reg_ID === q10Reg)))
+                          /*   if (regulation.reg_ID === q10Reg) {
+                              q10Reg = regulation.reg_ID+"/"+regulation.Ambito+"/"+regulation.Titulo+"/"+regulation.link
+                              console.log( regulation.reg_ID, q10Reg)
+                            } */
+                        }
+                        )
                         })
                       }
                       if (questions.q11) {
                         questions.q11.map((q11Reg:any) =>{
-                         //console.log("q11",q11Reg, this.loadRegulationsByID(q11Reg))
+                          this.regulationsList.map((regulation:regulationsDTO) => {
+                            console.log (regulation.reg_ID, q11Reg, ((regulation.reg_ID === q11Reg)))
+                          /*   if (regulation.reg_ID === q11Reg) {
+                              q11Reg = regulation.reg_ID+"/"+regulation.Ambito+"/"+regulation.Titulo+"/"+regulation.link
+                              console.log( regulation.reg_ID, q11Reg)
+                            } */
+                        }
+                        )
                         })
                       }  
                     })
@@ -117,7 +188,6 @@ export class GlobalRegulationQuestionnaireAnswerComponent {
     this.enviromentalAuditService.getRegulations()
       .subscribe( (regulations: any[]) => {
         this.regulationsList = regulations
-        console.log (this.regulationsList)
       })
 }
 }

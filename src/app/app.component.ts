c762b389afff2f7a72b500b42f795c82338b3ffd
Ignacio Lladó Vidal <ignacio.llado@idi.es>
2023-12-16 13:56:50 +0100

    haciendo el formulario de registro para usuarios externos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog-uoc-project-front';
+  currentYear: number
+  constructor() {
+    this.currentYear  = new Date().getFullYear();
+  }
 }

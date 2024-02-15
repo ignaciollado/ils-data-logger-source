32cec8efa0422c50a23b88b60db8359118ea817e
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2024-02-15 13:46:55 +0100

    estoy con el formulario de registro

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -73,7 +73,7 @@ export class RegisterComponent implements OnInit {
     this.confirmPassword = new UntypedFormControl(this.registerUser.password, [
       Validators.required,
       Validators.minLength(8),
-      Validators.maxLength(25),
+      Validators.maxLength(55),
     ]);
 
 
